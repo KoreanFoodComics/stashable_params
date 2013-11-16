493bbd072d4f073a1bfed9ff5b533973a8f66b80
Lin Reid <linreid@gmail.com>
2013-11-15 20:54:08 -0500

    Implement params filter * Add default filter for omiting sensitive params from stashed_params * Default filter can be redefined to filter out specific keys * Filters out nested params keys

--- lib/stashable_params.rb
+++ lib/stashable_params.rb
@@ -2,6 +2,26 @@ require "stashable_params/version"
 
 module StashableParams
   def stash_params
-    session[:stashed_params] = params
+    session[:stashed_params] = filter_params(params, params_filter)
+  end
+
+  def unstash_params
+    params.merge!(session.delete(:stashed_params)) if session[:stashed_params]
+  end
+
+  def filter_params(hash, filter)
+    filtered_hash = hash
+    filtered_hash.each do |k, v|
+      if filter.include?(k) || filter.include?(k.to_sym)
+        filtered_hash.delete(k)
+      else
+        filter_params(v, filter) if v.is_a?(Hash)
+      end
+    end
+    filtered_hash
+  end
+
+  def params_filter
+    [:password, :password_confirmation, :action, :controller]
   end
 end
--- test/controller_test.rb
+++ test/controller_test.rb
@@ -2,14 +2,18 @@ require 'test_helper'
 require 'byebug'
 
 class ApplicationController
-  include StashableParams
   class << self; attr_accessor :session end
   attr_accessor :params
 
-  @@session = {}
+  @@session = {}      # Mimics session available from all controllers in rails app
 
   def initialize
-    @params  = {}
+    @params  = {}     # Mimics params hash available within controllers in rails app
+  end
+
+  def session
+    # Gives easy access to the session to class instances
+    self.class.session
   end
 
   def self.session
@@ -28,25 +32,85 @@ describe ApplicationController do
   end
 end
 
-class TestController < ApplicationController
-  def session
-    self.class.session
-  end
+class StashableController < ApplicationController
+  include StashableParams
 
   def stash_params_action
     stash_params
   end
+
+  def unstash_params_action
+    unstash_params
+  end
+
+  def self.reset_default_params_filter
+    def params_filter
+      [:password, :password_confirmation, :action, :controller]
+    end
+  end
 end
 
-describe TestController do
-  let(:params) { { test: 'params', password: 'password', other_param: 'save me!', sensitive_param: 'dont save me!' } }
-  let(:test_controller) { TestController.new }
+describe StashableController do
+  let(:params) { { normal_param_key: 'params',
+                   password: 'password',
+                   sensitive_param: 'dont save me!',
+                   nested_hash: { nested_key: "I'm nested!" } } }
+
+  let(:stashable_controller) { StashableController.new }
 
   it 'can stash params' do
-    test_controller.params = params
-    test_controller.params.must_equal(params)
+    stashable_controller.params = params
 
-    test_controller.stash_params_action
+    stashable_controller.stash_params_action
     ApplicationController.session[:stashed_params].must_equal params
+  end
+
+  it 'can unstash params' do
+    stashable_controller.params = params
+    stashable_controller.stash_params_action
+    stashable_controller.params.must_equal(params)
+    stashable_controller.params = {}
+    stashable_controller.params.must_equal({})
+
+    stashable_controller.unstash_params_action
+
+    ApplicationController.session[:stashed_params].must_equal {}
+    stashable_controller.params.must_equal(params)
+  end
+
+  it 'provides a default filter for filtering out sensitive params' do
+    stashable_controller.params = params
+    stashable_controller.stash_params_action
+
+    ApplicationController.session[:stashed_params].wont_include(:password)
+    ApplicationController.session[:stashed_params].must_include(:normal_param_key)
+  end
+
+  it 'can redefine the filter to omit additional params' do
+    class StashableController < ApplicationController
+      def params_filter
+        [:sensitive_param]
+      end
+    end
+
+    stashable_controller.params = params
+    stashable_controller.stash_params_action
+
+    ApplicationController.session[:stashed_params].wont_include(:sensitive_param)
+    StashableController.reset_default_params_filter
+  end
+
+  it 'filters nested parameter keys' do
+    class StashableController < ApplicationController
+      def params_filter
+        [:nested_key]
+      end
+    end
+
+    stashable_controller.params = params
+    stashable_controller.stash_params_action
+
+    ApplicationController.session[:stashed_params][:nested_hash].wont_include(:nested_key)
+    StashableController.reset_default_params_filter
   end
 end
